06700638f5c2659c2af23968b67c5ee80e6ba69c
Phan Thi Hai Ha <phanha.leti@gmail.com>
2021-01-25 21:20:16 +0300

    updated function fo generate big prime numbers

--- CP_lab4.py
+++ CP_lab4.py
@@ -1,8 +1,80 @@
 import random
-p = 57896044618658097711785492504343953926634992332
-820282019728792003956564821041
-q = 5789604461865809771178549250434395392708293458372545
-0622380973592137631069619
+
+def MillerRabin(n):
+        if n!=int(n):
+            return False
+        n=int(n)
+        #Miller-Rabin test for prime
+        if n==0 or n==1 or n==4 or n==6 or n==8 or n==9:
+            return False 
+        if n==2 or n==3 or n==5 or n==7:
+            return True
+        s = 0
+        d = n-1
+        while d%2==0:
+            d>>=1
+            s+=1
+        assert(2**s * d == n-1)
+ 
+        def trial_composite(a):
+            if pow(a, d, n) == 1:
+                return False
+            for i in range(s):
+                if pow(a, 2**i * d, n) == n-1:
+                    return False
+            return True   
+        for i in range(8):#number of trials 
+            a = random.randrange(2, n)
+            if trial_composite(a):
+                return False 
+        return True 
+def mod(a,p):                 #приведение по модулю
+    mod = (a % p + p) % p
+    return mod
+
+def fast_pow(a, w, n):
+    s = 1
+    v = w
+    c = a
+    while not v == 0:
+        if mod(v,2) == 1:
+            s = mod(s*c,n)
+            v = (v-1)//2
+            c = mod(c**2,n)
+        else:
+            v = v//2
+            c = mod(c*c,n)
+    return s
+
+def getPrimeFactors(n):
+    prime_factor=[]
+    while n%2==0:
+        prime_factor.append(2)
+        n=n/2
+    for i in range(3, int(math.sqrt(n))+1,2):
+        while n%i==0:
+            prime_factor.append(int(i))
+            n=n/i
+    if n>2:
+        prime_factor.append(int(n))
+    return prime_factor
+l_p = int(input("Enter length bits of prime p -> "))
+l_q=int(l_p/12.8)
+q=random.randint((2**l_q)/2,(2**l_q)-1)
+while not MillerRabin(q):
+    q=random.randint((2**l_q)/2,(2**l_q)-1)
+i=1
+p=4
+dif =int(l_p-l_q)
+while not MillerRabin(p): #p=2*r*q+1
+    p=q*2
+    r=random.randint((2**dif)//2,(2**dif)-1) #r- нечетное число 
+    p=int(p*r)+1
+    i+=1
+print ("p=",p)
+print("q=",q)
+
+
 n = p * q
 s = random.randint(1, n - 1)  # private key
 y = (s**2) % n  # public key
